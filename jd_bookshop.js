d075fc5966dbb4c4644c562c17bd1f54a9481d8a
y785201595 <68041856+y785201595@users.noreply.github.com>
2021-07-01 12:59:16 +0800

    book

--- jd_bookshop.js
+++ jd_bookshop.js
@@ -32,8 +32,7 @@ ADD_CART = $.isNode() ? (process.env.PURCHASE_SHOPS ? process.env.PURCHASE_SHOPS
 // 加入购物车开关，与东东小窝共享
 
 let inviteCodes = [
-  '28a699ac78d74aa3b31f7103597f8927@2f14ee9c92954cf79829320dd482bf49@fdf827db272543d88dbb51a505c2e869@ce2536153a8742fb9e8754a9a7d361da@38ba4e7ba8074b78851e928af2b4f6b2',
-  '28a699ac78d74aa3b31f7103597f8927@2f14ee9c92954cf79829320dd482bf49@fdf827db272543d88dbb51a505c2e869'
+  ''
 ]
 
 if ($.isNode()) {
@@ -108,7 +107,7 @@ async function jdBeauty() {
   }
   if($.userInfo.storeGold) await chargeGold()
   await helpFriends()
-  // await showMsg();
+  await showMsg();
 }
 
 async function helpFriends() {
@@ -700,7 +699,7 @@ function shareCodesFormat() {
       const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
       $.newShareCodes = inviteCodes[tempIndex].split('@');
     }
-    const readShareCodeRes = await readShareCode();
+    const readShareCodeRes = null //await readShareCode();
     if (readShareCodeRes && readShareCodeRes.code === 200) {
       $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
     }
